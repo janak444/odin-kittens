4794ca306e0615be631c76443f2ff990dc681b67
janak444 <tripathi.janak@gmail.com>
2024-03-26 14:55:56 +0545

    complete api odin-kitten project

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @kittens }
+      format.json { render json: @kittens }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:now] = 'kitten saved successfully'
+      redirect_to @kitten
+    else
+      flash.alert = 'Data not found.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @kitten }
+      format.json { render json: @kitten }
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:alert] = 'Kitten update successfully.'
+      redirect_to @kitten
+    else
+      flash.alert = 'Data not found.'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
